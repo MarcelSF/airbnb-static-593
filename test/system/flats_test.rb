438225d8c9b1a7cf7720b2219ca1dce557a4c1ec
Marcel Fonseca <marcelsf23br@gmail.com>
2021-05-17 18:05:30 +0200

    all good

--- test/system/flats_test.rb
+++ test/system/flats_test.rb
@@ -6,4 +6,10 @@ class FlatsTest < ApplicationSystemTestCase
 
     assert_selector "h1", text: "Flats for everyone!"
   end
+
+    test "visiting the show page" do
+    visit "/flats/145"
+
+    assert_selector "h1", text: "Charm at the Steps of the Sacre Coeur/Montmartre"
+  end
 end
